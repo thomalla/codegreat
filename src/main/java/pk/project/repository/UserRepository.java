47d4bb6a824e4ad9475d7e0655f988cd9e4f90f0
LukaszHamowski <lukaszhamowski@gmail.com>
2017-05-29 17:19:07 +0200

    Removed unused import

--- src/main/java/pk/project/repository/UserRepository.java
+++ src/main/java/pk/project/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package pk.project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import pk.project.model.User;
 
 import java.util.Optional;
