7fe60aa92e781f1f35248b6d5564880699612056
filip gurgul <filipf6@gmail.com>
2017-05-30 02:16:21 +0200

    logging in and logging out working

--- src/main/java/pk/project/controller/HomeController.java
+++ src/main/java/pk/project/controller/HomeController.java
@@ -1,28 +1,32 @@
 package pk.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pk.project.model.CurrentUser;
+import pk.project.service.UserService;
 import pk.project.util.Path.Web;
 import pk.project.util.Path.Template;
+
+import java.util.NoSuchElementException;
 
 @Controller
 public class HomeController
 {
-
-    //@ModelAttribute("currentUser")
     @RequestMapping(Web.INDEX)
-    public String serveIndexPage(Authentication authentication)
+    public String serveIndexPage(Model model, Authentication authentication)
     {
         if(authentication==null) System.out.println("nie ma uzytnika");
         else
         {
             CurrentUser currentUser = (CurrentUser) authentication.getPrincipal();
-            System.out.println(currentUser.toString());
+            model.addAttribute("user",currentUser.getUser());
         }
 
         return Template.INDEX;
