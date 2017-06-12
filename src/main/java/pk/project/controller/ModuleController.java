e13130a502fb96d0b546d7c07b85213a92314041
thomalla <zamoratomek@gmail.com>
2017-06-12 21:44:47 +0200

    test templates, modules added

--- src/main/java/pk/project/controller/ModuleController.java
+++ src/main/java/pk/project/controller/ModuleController.java
@@ -15,10 +15,22 @@ public class ModuleController
         return Path.Template.ALLMODULES;
     }
 
-    @RequestMapping(value = Path.Web.DESCRIPTION11)
-    public String serveDescription11Page()
+    @RequestMapping(value = Path.Web.DESCRIPTION1)
+    public String serveDescription1Page()
     {
-        return Path.Template.DESCRIPTION11;
+        return Path.Template.DESCRIPTION1;
+    }
+
+    @RequestMapping(value = Path.Web.DESCRIPTION2)
+    public String serveDescription2Page()
+    {
+        return Path.Template.DESCRIPTION2;
+    }
+
+    @RequestMapping(value = Path.Web.DESCRIPTION3)
+    public String serveDescription3Page()
+    {
+        return Path.Template.DESCRIPTION3;
     }
 
     @RequestMapping(value = Path.Web.LESSON11)
@@ -93,16 +105,102 @@ public class ModuleController
     {
         return Path.Template.LESSON34;
     }
-
     @RequestMapping(value = Path.Web.LESSON35)
     public String serveLesson35Page()
     {
         return Path.Template.LESSON35;
     }
 
+    @RequestMapping(value = Path.Web.TEST1)
+    public String serveTest1Page()
+    {
+        return Path.Template.TEST1;
+    }
+    @RequestMapping(value = Path.Web.TEST2)
+    public String serveTest2Page()
+    {
+        return Path.Template.TEST2;
+    }
+    @RequestMapping(value = Path.Web.TEST3)
+    public String serveTest3Page() { return Path.Template.TEST3; }
+
+
     @RequestMapping(value = Path.Web.TEST11)
     public String serveTest11Page()
     {
         return Path.Template.TEST11;
     }
+    @RequestMapping(value = Path.Web.TEST12)
+    public String serveTest12Page()
+    {
+        return Path.Template.TEST12;
+    }
+    @RequestMapping(value = Path.Web.TEST13)
+    public String serveTest13Page()
+    {
+        return Path.Template.TEST13;
+    }
+    @RequestMapping(value = Path.Web.TEST14)
+    public String serveTest14Page()
+    {
+        return Path.Template.TEST14;
+    }
+    @RequestMapping(value = Path.Web.TEST15)
+    public String serveTest15Page()
+    {
+        return Path.Template.TEST15;
+    }
+    @RequestMapping(value = Path.Web.TEST16)
+    public String serveTest16Page()
+    {
+        return Path.Template.TEST16;
+    }
+
+    @RequestMapping(value = Path.Web.TEST21)
+    public String serveTest21Page()
+    {
+        return Path.Template.TEST21;
+    }
+    @RequestMapping(value = Path.Web.TEST22)
+    public String serveTest22Page()
+    {
+        return Path.Template.TEST22;
+    }
+    @RequestMapping(value = Path.Web.TEST23)
+    public String serveTest23Page()
+    {
+        return Path.Template.TEST23;
+    }
+    @RequestMapping(value = Path.Web.TEST24)
+    public String serveTest24Page()
+    {
+        return Path.Template.TEST24;
+    }
+    @RequestMapping(value = Path.Web.TEST25)
+    public String serveTest25Page()
+    {
+        return Path.Template.TEST25;
+    }
+
+    @RequestMapping(value = Path.Web.TEST31)
+    public String serveTest31Page()
+    {
+        return Path.Template.TEST31;
+    }
+    @RequestMapping(value = Path.Web.TEST32)
+    public String serveTest32Page()
+    {
+        return Path.Template.TEST32;
+    }
+    @RequestMapping(value = Path.Web.TEST33)
+    public String serveTest33Page()
+    {
+        return Path.Template.TEST33;
+    }
+    @RequestMapping(value = Path.Web.TEST34)
+    public String serveTest34Page()
+    {
+        return Path.Template.TEST34;
+    }
+
 }
--- src/main/java/pk/project/util/Path.java
+++ src/main/java/pk/project/util/Path.java
@@ -18,9 +18,9 @@ public class Path
         public static final String ADD_QUESTION = "/addQuestion";
         public static final String ALLMODULES = "/allModules";
 
-        public static final String DESCRIPTION11 = "/description/1/1";
-        public static final String DESCRIPTION12 = "/description/1/2";
-        public static final String DESCRIPTION13 = "/description/1/3";
+        public static final String DESCRIPTION1 = "/description/1";
+        public static final String DESCRIPTION2 = "/description/2";
+        public static final String DESCRIPTION3 = "/description/3";
 
 
         public static final String LESSON11 = "/lesson/1/1";
@@ -40,7 +40,29 @@ public class Path
         public static final String LESSON33 = "/lesson/3/3";
         public static final String LESSON34 = "/lesson/3/4";
         public static final String LESSON35 = "/lesson/3/5";
+
+        public static final String TEST1 = "/test/1";
+        public static final String TEST2 = "/test/2";
+        public static final String TEST3 = "/test/3";
+
         public static final String TEST11 = "/test/1/1";
+        public static final String TEST12 = "/test/1/2";
+        public static final String TEST13 = "/test/1/3";
+        public static final String TEST14 = "/test/1/4";
+        public static final String TEST15 = "/test/1/5";
+        public static final String TEST16 = "/test/1/6";
+
+        public static final String TEST21 = "/test/2/1";
+        public static final String TEST22 = "/test/2/2";
+        public static final String TEST23 = "/test/2/3";
+        public static final String TEST24 = "/test/2/4";
+        public static final String TEST25 = "/test/2/5";
+
+        public static final String TEST31 = "/test/3/1";
+        public static final String TEST32 = "/test/3/2";
+        public static final String TEST33 = "/test/3/3";
+        public static final String TEST34 = "/test/3/4";
+
 
         public static final String ACCOUNTEDITION = "/accountEdition";
         public static final String DELETEACCOUNTCONFIRMATION="/deleteAccountConfirmation";
@@ -68,9 +90,9 @@ public class Path
         public static final String QUESTION_POSTED = "html/questionPosted.html";
         public static final String ALLMODULES = "html/allModules.html";
 
-        public static final String DESCRIPTION11 = "html/course/description1.html";
-        public static final String DESCRIPTION12 = "html/course/description2.html";
-        public static final String DESCRIPTION13 = "html/course/description3.html";
+        public static final String DESCRIPTION1 = "html/course/description1.html";
+        public static final String DESCRIPTION2 = "html/course/description2.html";
+        public static final String DESCRIPTION3 = "html/course/description3.html";
 
         public static final String LESSON11 = "html/course/lessons/lesson11.html";
         public static final String LESSON12 = "html/course/lessons/lesson12.html";
@@ -78,16 +100,39 @@ public class Path
         public static final String LESSON14 = "html/course/lessons/lesson14.html";
         public static final String LESSON15 = "html/course/lessons/lesson15.html";
         public static final String LESSON16 = "html/course/lessons/lesson16.html";
+
         public static final String LESSON21 = "html/course/lessons/lesson21.html";
         public static final String LESSON22 = "html/course/lessons/lesson22.html";
         public static final String LESSON23 = "html/course/lessons/lesson23.html";
         public static final String LESSON24 = "html/course/lessons/lesson24.html";
+
         public static final String LESSON31 = "html/course/lessons/lesson31.html";
         public static final String LESSON32 = "html/course/lessons/lesson32.html";
         public static final String LESSON33 = "html/course/lessons/lesson33.html";
         public static final String LESSON34 = "html/course/lessons/lesson34.html";
         public static final String LESSON35 = "html/course/lessons/lesson35.html";
-        public static final String TEST11 = "html/tests/test11.html";
+
+        public static final String TEST1 = "html/course//tests/test1.html";
+        public static final String TEST2 = "html/course//tests/test2.html";
+        public static final String TEST3 = "html/course//tests/test3.html";
+
+        public static final String TEST11 = "html/course//tests/test11.html";
+        public static final String TEST12 = "html/course//tests/test12.html";
+        public static final String TEST13 = "html/course//tests/test13.html";
+        public static final String TEST14 = "html/course//tests/test14.html";
+        public static final String TEST15 = "html/course//tests/test15.html";
+        public static final String TEST16 = "html/course//tests/test16.html";
+
+        public static final String TEST21 = "html/course//tests/test21.html";
+        public static final String TEST22 = "html/course//tests/test22.html";
+        public static final String TEST23 = "html/course//tests/test23.html";
+        public static final String TEST24 = "html/course//tests/test24.html";
+        public static final String TEST25 = "html/course//tests/test25.html";
+
+        public static final String TEST31 = "html/course//tests/test31.html";
+        public static final String TEST32 = "html/course//tests/test32.html";
+        public static final String TEST33 = "html/course//tests/test33.html";
+        public static final String TEST34 = "html/course//tests/test34.html";
 
         public static final String ACCOUNTEDITION = "html/accountEdition.html";
         public static final String DELETEACCOUNTCONFIRMATION = "html/deleteAccountConfirmation.html";
